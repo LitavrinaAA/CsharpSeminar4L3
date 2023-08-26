3a444672a77c1a9048a74876fcf5c41de7ab54b5
LitavrinaAA <a.a.litavrina@google.com>
2023-08-26 20:14:42 +0300

    Summ all nums from 1 to input N

---
+++ ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿// 
+// one more solution
+int InputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumNum (int num){
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+        sum += i;
+        return sum;
+    
+}
+int num = InputNum("Input num: ");
+int sum = SumNum(num);
+System.Console.WriteLine(sum);
