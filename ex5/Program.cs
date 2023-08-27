5e85444a43a57688f376ba10cf48d968bbcc28a6
LitavrinaAA <a.a.litavrina@google.com>
2023-08-27 09:13:24 +0300

    Qwantity of digits in a number.Solution with for

---
+++ ex5/Program.cs
@@ -0,0 +1,16 @@
+﻿int InputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+int numDigits(int num)
+{
+ int i;
+    for (i = 0; num != 0; i++, num /= 10) ;
+    return i;   
+
+}
+
+int num = InputNum("Input number: ");
+
+System.Console.WriteLine(numDigits(num));
