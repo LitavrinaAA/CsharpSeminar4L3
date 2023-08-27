a69641d8084810693bf23dd3e2b6c65269f053c1
LitavrinaAA <a.a.litavrina@google.com>
2023-08-27 09:04:57 +0300

    Qwantity of digits in a number

---
+++ ex4/Program.cs
@@ -0,0 +1,19 @@
+﻿//принимает число и в нем количество цифр
+int InputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int numDigits(int num){
+    int count = 0;
+    while(num != 0) {
+        num = num/10;
+        //System.Console.WriteLine(num);
+        count++;
+    }
+    return count;
+}
+int i = InputNum("Input num: ");
+System.Console.WriteLine(numDigits(i));
+//System.Console.WriteLine(i);
