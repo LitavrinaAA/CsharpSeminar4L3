183be37247fcfc9e313c4e4da341b03a4c3b2c80
LitavrinaAA <a.a.litavrina@google.com>
2023-08-27 14:26:10 +0300

    Factorial

---
+++ ex6/Program.cs
@@ -0,0 +1,17 @@
+﻿// 28 to accept number N, return multiple from 1 to N
+int InputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int FactorialN(int num){
+    int mult = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        mult *= i;
+    }
+    return mult;
+}
+int num = InputNum("Input num: ");
+System.Console.WriteLine(FactorialN(num));
